0bf46b157d346a88fcf1a09a6dfa1b371c91ffdd
Daniel Kristiansen <danielkris96@gmail.com>
2025-11-03 07:49:20 +0100

    linting for markdown/docker/json/text

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -11,36 +11,25 @@ return {
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
+      lint.linters_by_ft = lint.linters_by_ft or {}
       --
       -- However, note that this will enable a set of default linters,
       -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
+      lint.linters_by_ft = {
+        text = { 'vale' },
+        dockerfile = { 'hadolint' },
+        json = { 'jsonlint' },
+        markdown = { 'vale' },
+        --   clojure = { "clj-kondo" },
+        --   inko = { "inko" },
+        --   janet = { "janet" },
+        --   rst = { "vale" },
+        --   ruby = { "ruby" },
+        --   terraform = { "tflint" },
+      }
       --
       -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
       -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
-
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
@@ -55,6 +44,9 @@ return {
           end
         end,
       })
+      vim.api.nvim_create_user_command('Lint', function()
+        lint.try_lint()
+      end, { desc = 'Run linters manually' })
     end,
   },
 }
